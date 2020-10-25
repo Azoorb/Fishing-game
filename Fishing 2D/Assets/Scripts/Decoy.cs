9fbcf88b03360889bcf50e5770780d5aa7557276
Leo Dorn <leo.dorn@etudiant.univ-lr.fr>
2020-10-25 10:20:21 +0100

    Rangement + début light

---
+++ Fishing 2D/Assets/Scripts/Decoy.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decoy : MonoBehaviour
+{
+    [SerializeField,Range(-20,0)]
+    float forceToGoDownInWater;
+    [SerializeField]
+    float depthLimit;
+    float touchWaterPoint;
+    [HideInInspector]
+    public GameObject fish;
+    [SerializeField]
+    GameObject deadFishPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        depthLimit = Player.instance.depthLimit;
+        touchWaterPoint = 999;
+        transform.position = new Vector3(transform.position.x, transform.position.y, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(touchWaterPoint != 999)
+        {
+            
+            if (Mathf.Abs(touchWaterPoint-transform.position.y)>=depthLimit)
+            {
+                GetComponent<Rigidbody2D>().velocity =
+                    new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, forceToGoDownInWater);
+            }
+        }
+        CreateLine(Player.instance.transform.position);
+        Collider2D[] listCollider = Physics2D.OverlapCircleAll(transform.position,0.5f);
+        foreach(Collider2D collider in listCollider)
+        {
+            if(collider.CompareTag("End"))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+            }
+        }
+    }
+
+    void CreateLine(Vector2 positionToCreateLine)
+    {
+        LineRenderer lineRenderer = transform.GetChild(0).GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, positionToCreateLine);
+        lineRenderer.SetPosition(1, transform.position);
+        lineRenderer.startWidth = 0.03f;
+        lineRenderer.endWidth = 0.03f;
+    }
+    
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Water"))
+        {
+            Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+            rigidBody.velocity = new Vector2(0, forceToGoDownInWater);
+            rigidBody.gravityScale = 0;
+            touchWaterPoint = collision.transform.position.y;
+        }
+    }
+
+
+    public void GetBackFish()
+    {
+        Player.instance.isFishing = false;
+        Player.instance.rideUp = false;
+        if (fish != null)
+        {
+            if (Player.instance.fishCaughtList.Count < 3 && !Player.instance.inBoat)
+            {
+                GameObject pointToStock = GameObject.Find("PointToStockFishNoBoat");
+                StockFish(pointToStock.transform);
+                Player.instance.fishCaughtList.Add(fish.GetComponent<Fish>());
+            }
+            else if(Player.instance.inBoat && Player.instance.boat.GetComponent<Boat>().stockInBoatForFish> Player.instance.boat.GetComponent<Boat>().fishCaughtList.Count)
+            {
+                StockFish(Player.instance.boat.GetComponent<Boat>().fishStockPlaceTransform);
+                Player.instance.boat.GetComponent<Boat>().fishCaughtList.Add(fish.GetComponent<Fish>());
+            }
+
+            FishManager.instance.DestroyFish(fish.GetComponent<Fish>());
+
+            
+        }
+        Player.instance.AttachCameraAndSetPosition(Player.instance.transform);
+        Destroy(gameObject);
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+        
+    }
+
+    void StockFish(Transform place)
+    {
+        GameObject deadFish;
+        if(Player.instance.inBoat)
+        {
+            deadFish = Instantiate(deadFishPrefab,
+            new Vector2(place.position.x,
+            (float)(place.position.y + 0.25 * Player.instance.boat.GetComponent<Boat>().fishCaughtList.Count)),
+            Quaternion.identity); 
+        }
+        else
+        {
+            deadFish = Instantiate(deadFishPrefab,
+            new Vector2(place.position.x,
+            (float)(place.position.y + 0.25 * Player.instance.fishCaughtList.Count)),
+            Quaternion.identity);
+        }
+        
+        deadFish.GetComponent<SpriteRenderer>().sprite = fish.GetComponent<Fish>().deadSprite;
+
+        deadFish.transform.localScale = fish.transform.lossyScale;
+        deadFish.transform.SetParent(place.transform);
+    }
+
+    public bool InWaterOrNot()
+    {
+        if(touchWaterPoint == 999)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
---
+++ Fishing 2D/Assets/Scripts/Player.cs
@@ -0,0 +1,279 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+    [SerializeField]
+    float speed;
+    PlayerController controller;
+    float axisH;
+    float movementValue;
+    [SerializeField]
+    GameObject decoyPrefab, bar;
+    [HideInInspector]
+    public bool goodShoot;
+    [SerializeField]
+    Transform spawnDecoyTransform;
+    [SerializeField]
+    Vector2 directionToApplyToDecoy;
+    [SerializeField]
+    float forceToApplyToDecoy;
+    [HideInInspector]
+    public bool isFishing = false;
+    [SerializeField]
+    GameObject cameraScene;
+    GameObject decoy;
+    [HideInInspector]
+    public bool rideUp;
+    [HideInInspector]
+    public List<Fish> fishCaughtList;
+    [HideInInspector]
+    public bool inBoat = false;
+    [HideInInspector]
+    public GameObject boat;
+    Ground groundNear;
+    [HideInInspector]
+    public int money;
+    [HideInInspector]
+    public bool inShop = false;
+    public float depthLimit;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        fishCaughtList = new List<Fish>();
+        controller = new PlayerController();
+        controller.Player.Move.performed += ctx =>
+        {
+            if (inBoat)
+            {
+                boat.GetComponent<Boat>().valueToMove = ctx.ReadValue<float>();
+            }
+            else
+            {
+                movementValue = ctx.ReadValue<float>();
+            }
+
+        };
+        controller.Player.Move.canceled += ctx =>
+        {
+            if (inBoat)
+            {
+                boat.GetComponent<Boat>().valueToMove = 0;
+            }
+            else
+            {
+                movementValue = 0;
+            }
+        };
+        
+        controller.Player.LaunchDecoy.performed += ctx =>
+        {
+            if (!isFishing && CheckIfColliderAround(2,"Water"))
+            {
+                StartFishing();
+            }
+            else if(!CheckIfColliderAround(2, "Water"))
+            {
+                Debug.Log("Vous êtes trop loin de l'eau");
+            }
+            
+               
+        };
+        controller.Player.RideUpDecoy.performed += ctx =>
+        {
+            if (isFishing)
+            {
+                rideUp = true;
+            }
+        };
+        controller.Player.RideUpDecoy.canceled += ctx =>
+        {
+            rideUp = false;
+        };
+    }
+    void Start()
+    {
+        AttachCameraAndSetPosition(transform);
+    }
+
+    bool CheckIfColliderAround(float radius,string tag)
+    {
+        Collider2D[] listCollider = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach (Collider2D collider in listCollider)
+        {
+            if (collider.CompareTag(tag))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void AttachCameraAndSetPosition(Transform transformToAttach)
+    {
+        cameraScene.transform.SetParent(transformToAttach);
+        cameraScene.transform.position = new Vector3(transformToAttach.position.x, 
+            transformToAttach.position.y, cameraScene.transform.position.z);
+        
+    }
+
+    void Move(float moveValue)
+    {
+        transform.Translate(Vector2.right * moveValue * speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inBoat)
+        {
+            transform.position = boat.GetComponent<Boat>().navigationPlaceTransform.position;
+            Collider2D[] listCollider = Physics2D.OverlapCircleAll(transform.position, 5);
+            Ground groundNearTemp = null;
+            foreach (Collider2D collider in listCollider)
+            {
+                if (collider.CompareTag("Ground"))
+                {
+                    groundNearTemp = collider.GetComponent<Ground>();
+                }
+            }
+            if (groundNearTemp == null)
+            {
+                groundNear = null;
+                UIManager.instance.SetActiveButtonGoOutOfBoat(false);
+            }
+            else if(!inShop)
+            {
+                groundNear = groundNearTemp;
+                UIManager.instance.SetActiveButtonGoOutOfBoat(true);
+            }
+        }
+        else if (!inBoat)
+        {            
+            
+            if (!CheckIfColliderAround(5,"Boat"))
+            {
+                UIManager.instance.SetActiveButtonGoInBoat(false);
+            }
+            else if(!inShop)
+            {
+                UIManager.instance.SetActiveButtonGoInBoat(true);
+            }
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isFishing || !inBoat)
+        {
+            Move(movementValue);
+        }
+
+        if (rideUp && decoy != null && decoy.GetComponent<Decoy>().InWaterOrNot())
+        {
+            decoy.transform.position = Vector2.MoveTowards(decoy.transform.position, transform.position, 11 * Time.deltaTime);
+        }
+        
+    }
+
+
+    private void OnEnable()
+    {
+        controller.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controller.Player.Disable();
+    }
+
+   
+    void StartFishing()
+    {
+        
+
+        if (bar.activeSelf == false)
+        {
+            bar.SetActive(true);
+            bar.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Launch");
+        }
+        else if(bar.activeSelf == true )
+        {
+            
+            if (goodShoot)
+            {
+
+                LaunchDecoy(2);
+            }
+            else
+            {
+                LaunchDecoy(1);
+            }
+            isFishing = true;
+        }
+        
+    }
+
+    void LaunchDecoy(int forceToMultiply)
+    {
+        decoy  = Instantiate(decoyPrefab, spawnDecoyTransform.position, Quaternion.identity);
+        decoy.GetComponent<Rigidbody2D>().AddForce(directionToApplyToDecoy * forceToApplyToDecoy * forceToMultiply);
+        bar.SetActive(false);
+        AttachCameraAndSetPosition(decoy.transform);
+    }
+
+    
+
+    public void FailFishing()
+    {
+        bar.SetActive(false);
+        isFishing = false;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Decoy"))
+        {
+            collision.GetComponent<Decoy>().GetBackFish();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 5);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 2);
+    }
+
+    public void GoInBoat()
+    {
+        inBoat = true;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        transform.position = boat.GetComponent<Boat>().navigationPlaceTransform.position;
+        transform.SetParent(boat.transform);
+        
+    }
+
+    public void GoOutOfBoat()
+    {
+
+        transform.SetParent(null);
+        inBoat = false;
+        transform.position = groundNear.pointToAccost.transform.position;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        //J'sais pas pourquoi il se déplaçait de temps en temps
+        movementValue = 0;
+
+    }
+
+
+}
